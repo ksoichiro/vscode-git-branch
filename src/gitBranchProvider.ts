6a24c55a5092d4ad5dda7a8fc41c7b3a5edd1fcb
Soichiro Kashima <soichiro.kashima@gmail.com>
2020-10-06 23:55:42 +0900

    Add group layer and tags list

--- src/gitBranchProvider.ts
+++ src/gitBranchProvider.ts
@@ -4,10 +4,10 @@ import { API, Repository, Ref, RefType } from './api/git';
 export class GitBranchProvider implements vscode.TreeDataProvider<GitBranch> {
     private _onDidChangeTreeData: vscode.EventEmitter<GitBranch | undefined | void> = new vscode.EventEmitter<GitBranch | undefined | void>();
     readonly onDidChangeTreeData: vscode.Event<GitBranch | undefined | void> = this._onDidChangeTreeData.event;
-    private branches: GitBranches;
+    private entries: GitBranches;
 
     constructor(private api: API) {
-        this.branches = new GitBranches();
+        this.entries = new GitBranches();
         if (this.api.repositories) {
             const repository = this.api.repositories[0];
             repository.state.onDidChange(_ => {
@@ -25,9 +25,9 @@ export class GitBranchProvider implements vscode.TreeDataProvider<GitBranch> {
         if (element) {
             // TODO
             console.log("getChildren: " + JSON.stringify(element));
-            return Promise.resolve([]);
+            return Promise.resolve(element.children);
         }
-        return Promise.resolve(this.branches);
+        return Promise.resolve(this.entries);
     }
 
     checkout(gitBranch: GitBranch) {
@@ -44,9 +44,10 @@ export class GitBranchProvider implements vscode.TreeDataProvider<GitBranch> {
 
     private async updateBranches(repository: Repository) {
         var current = await repository.getBranch('HEAD');
+
         // Getting refs by repository.getBranches() triggers the update of repository.state,
         // which result in infinite updates, so filter and sort refs manually.
-        var refs = repository.state.refs
+        var refsHead = repository.state.refs
             .filter(p => p.type === RefType.Head)
             .sort((a: Ref, b: Ref) => {
                 if (!a || !a.name || !b || !b.name) {
@@ -54,17 +55,41 @@ export class GitBranchProvider implements vscode.TreeDataProvider<GitBranch> {
                 }
                 return a.name.localeCompare(b.name);
             });
+        var refsTag = repository.state.refs
+            .filter(p => p.type === RefType.Tag)
+            .sort((a: Ref, b: Ref) => {
+                if (!a || !a.name || !b || !b.name) {
+                    return -1;
+                }
+                return b.name.localeCompare(a.name); // TODO sort by version, descending
+            });
+
+        var entries = new GitBranches();
+        var groupBranch = new GitBranch('Branches', vscode.TreeItemCollapsibleState.Expanded, '', false, GitBranchNodeType.Group, 'group.branches');
+        entries.push(groupBranch);
         var branches = new GitBranches();
-        refs.forEach((ref) => {
+        refsHead.forEach((ref) => {
             if (ref.name && ref.commit) {
-                branches.push(new GitBranch(ref.name, vscode.TreeItemCollapsibleState.None, ref.commit, current && current.name === ref.name));
+                branches.push(new GitBranch(ref.name, vscode.TreeItemCollapsibleState.None, ref.commit, current && current.name === ref.name, GitBranchNodeType.Branch, ''));
             }
         });
-        if (this.branches.equals(branches)) {
+        groupBranch.children = branches;
+        if (this.entries.equals(entries)) {
             // If branches are not changes, ignore it
             return;
         }
-        this.branches = branches;
+
+        var groupTag = new GitBranch('Tags', vscode.TreeItemCollapsibleState.Expanded, '', false, GitBranchNodeType.Group, 'group.tags');
+        entries.push(groupTag);
+        var tags = new GitBranches();
+        refsTag.forEach((ref) => {
+            if (ref.name && ref.commit) {
+                tags.push(new GitBranch(ref.name, vscode.TreeItemCollapsibleState.None, ref.commit, false, GitBranchNodeType.Tag, ''));
+            }
+        });
+        groupTag.children = tags;
+
+        this.entries = entries;
         this._onDidChangeTreeData.fire();
     }
 }
@@ -87,32 +112,53 @@ class GitBranches extends Array<GitBranch> {
     }
 }
 
+enum GitBranchNodeType {
+    Group,
+    Branch,
+    Tag,
+}
+
 export class GitBranch extends vscode.TreeItem {
+    public children: GitBranches;
+
     constructor(
         public readonly label: string,
         public readonly collapsibleState: vscode.TreeItemCollapsibleState,
         public readonly commit: string,
         public readonly current: boolean,
+        public readonly nodeType: GitBranchNodeType,
+        public readonly id: string,
     ) {
         super(label, collapsibleState);
+        this.children = new GitBranches();
         // icons can be selected from codicons:
         // https://code.visualstudio.com/api/references/icons-in-labels#icon-listing
         // https://microsoft.github.io/vscode-codicons/dist/codicon.html
-        this.iconPath = new vscode.ThemeIcon(current ? 'circle-filled' : 'git-branch');
-        if (current) {
-            this.contextValue = 'currentBranch';
-        } else {
-            this.contextValue = 'nonCurrentBranch';
-            this.command = {
-                title: '',
-                command: 'gitBranch.checkout',
-                tooltip: '',
-                arguments: [this.label],
-            };
+        if (this.nodeType === GitBranchNodeType.Branch) {
+            this.iconPath = new vscode.ThemeIcon(current ? 'circle-filled' : 'git-branch');
+            if (current) {
+                this.contextValue = 'currentBranch';
+            } else {
+                this.contextValue = 'nonCurrentBranch';
+                this.command = {
+                    title: '',
+                    command: 'gitBranch.checkout',
+                    tooltip: '',
+                    arguments: [this.label],
+                };
+            }
+        } else if (this.nodeType === GitBranchNodeType.Tag) {
+            this.iconPath = new vscode.ThemeIcon('tag');
         }
     }
 
     equal(obj: GitBranch): boolean {
-        return obj && this.label === obj.label && this.commit === obj.commit && this.current === obj.current;
+        return obj
+            && this.label === obj.label
+            && this.commit === obj.commit
+            && this.current === obj.current
+            && this.nodeType === obj.nodeType
+            && this.id === obj.id
+            && this.children.equals(obj.children);
     }
 }
